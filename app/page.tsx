608c873ec39a216d3891414660ea903f1be4342b
Dardan Kabashi <e12510771@student.tuwien.ac.at>
2026-04-06 02:23:03 +0200

    fix admin modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import { PatientForm } from "@/components/forms/PatientForm";
 import { PasskeyModal } from "@/components/PasskeyModal";
 
-const Home = ({ searchParams }: SearchParamProps) => {
-  const isAdmin = searchParams?.admin === "true";
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const params = await searchParams;
+  const isAdmin = params?.admin === "true";
 
   return (
     <div className="flex h-screen max-h-screen">
       {isAdmin && <PasskeyModal />}
-
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -20,9 +19,7 @@ const Home = ({ searchParams }: SearchParamProps) => {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
-
           <PatientForm />
-
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2024 CarePluse
@@ -33,7 +30,6 @@ const Home = ({ searchParams }: SearchParamProps) => {
           </div>
         </div>
       </section>
-
       <Image
         src="/assets/images/onboarding-img.png"
         height={1000}
